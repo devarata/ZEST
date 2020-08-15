41942818f91c271c7878a86afb8c4b5d7d47e20d
Ride0401 <devarata.oza@gmail.com>
2020-08-15 15:40:14 +0530

    app design

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Slack clone</h1>
+      {/*Header*/}
+      {/*Sidebar*/}
+      {/*React Router Chat screen*/}
     </div>
   );
+
+
 }
 
 export default App;
