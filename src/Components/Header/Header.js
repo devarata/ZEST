5383693de4676690e1bf4f7c275abcaa17ccd83a
Ride0401 <devarata.oza@gmail.com>
2020-08-15 15:56:10 +0530

    header design

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css'
+
+
+function Header() {
+  return (
+    <div className="header">
+        <div className="header__left">
+          {/*Avatar for logged in user*/}
+          {/*Time icon*/}
+        </div>
+        <div className="header__search">
+          {/*Search icon*/}
+          {/*input icon*/}
+        </div>
+        <div className="header__right">
+          {/*help icon*/}
+        </div>
+    </div>
+  )
+}
+
+export default Header
