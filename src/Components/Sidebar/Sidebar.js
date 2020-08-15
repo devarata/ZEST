936858ad252005ff21bcb93aaa5cd92c7db2f3f7
Ride0401 <devarata.oza@gmail.com>
2020-08-16 01:06:41 +0530

    add channel working

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -55,7 +55,7 @@ function Sidebar() {
       <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel"/>
       {/*Connect to db and list all the channels*/}
       {channels.map(channel=>(
-        <SidebarOption title={channel.name} />
+        <SidebarOption title={channel.name} id={channel.id} />
       ))}
     </div>
   )
