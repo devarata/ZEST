c88cb668db1bcc7f40b8f7365a79628cfae3b07b
Ride0401 <devarata.oza@gmail.com>
2020-08-16 00:34:20 +0530

    printing chat room name

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header'
 import Sidebar from './Components/Sidebar/Sidebar'
+import {Route, BrowserRouter as Router,Switch} from  "react-router-dom"
+import Chat from './Components/Chat/Chat'
 
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <div className="app__body">
-        <Sidebar/>
-      </div>
+      <Router>
+        <Header/>
+
+        <div className="app__body">
+          <Sidebar/>
+
+          <Switch>
+          <Route path="/room/:roomId">
+          <Chat/>
+
+            <h1>Chat Screen</h1>
+          </Route>
+          <Route path="/">
+            <h1>Welcome</h1>
+          </Route>
+          </Switch>
+
+        </div>
+      </Router>
+
 
 
       {/*React Router Chat screen*/}
